fffdf34d589693a8fa41d350b0f78835f3ca06f2
Rory <rory@tripvisuals.com>
2026-08-13 18:15:08 -0300

    finalizacao da implementacao 13-08-26

--- catalogador-router.js
+++ catalogador-router.js
@@ -578,7 +578,6 @@ router.get('/export/csv', async (_, res) => {
 router.get('/events', async (req, res) => {
     res.setHeader('Content-Type',      'text/event-stream');
     res.setHeader('Cache-Control',     'no-cache');
-    res.setHeader('Connection',        'keep-alive');
     res.setHeader('X-Accel-Buffering', 'no');
     res.flushHeaders();
 
